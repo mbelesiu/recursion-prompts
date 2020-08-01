3f318ba9dd89bef6c76b6e999b823aa21d3b7909
mbelesiu <matt.belesiu@gmail.com>
2020-08-01 16:51:42 -0700

    factorial, sum of arry, sum array mult, and check even implemented

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,50 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n === 1 || n === 0){
+    return 1;
+  } else if (n < 0) {
+    return null;
+  }
+  return (n * factorial(n-1));
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if(array.length === 1) {
+    return array[0];
+  } else if (array.length === 0) {
+    return 0;
+  }
+  var arrLength = array.length - 1;
+
+  return array[arrLength] + sum(array.slice(0,arrLength));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+
+  for ( var i = 0; i < array.length; i ++) {
+    if(Array.isArray(array[i])) {
+      sum += arraySum(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if(n === 1) {
+    return false;
+  }
+  return isEven(Math.abs(n-2));
 };
 
 // 5. Sum all integers below a given integer.
